c0efbb260af9e963c2fefa8245707f1f8012faa6
Aijaz ahmad Dar <110050419+daraijaz1122@users.noreply.github.com>
2023-01-14 19:43:14 +0530

    Update script.js

--- js Date&Time/js/script.js
+++ js Date&Time/js/script.js
@@ -3,8 +3,8 @@ function updateTime() {
     var hr = dateTime.getHours();
     var min = dateTime.getMinutes();
     var sec = dateTime.getSeconds();
-    document.getElementsById("hour").innerHTML= hr;
-    document.getElementsById("minutes").innerHTML=min;
+    document.getElementById("hour").innerHTML= hr;
+    document.getElementById("minutes").innerHTML=min;
     document.getElementById("seconds").innerHTML=sec;
 }
 setInterval(updateTime(),1000);
